895b2bbf70354ad021d663be6a023bf872b062c0
Sahil <thesahilsinghh@gmail.com>
2024-10-15 02:07:38 +0530

    update transition

--- Day 1/Clock/script.js
+++ Day 1/Clock/script.js
@@ -1,16 +1,26 @@
 const secondHand = document.getElementById("second-hand");
 const hourHand = document.getElementById("hour-hand");
-const minuteHand=document.getElementById("minute-hand");
+const minuteHand = document.getElementById("minute-hand");
+
 function setTime() {
   let now = new Date();
+
   let seconds = (now.getSeconds() / 60) * 360 + 90;
-  let hour = (now.getHours() / 60) * 360 + 90;
-  let minutes=(now.getMinutes()/60)*360+90;
+  let minutes =
+    (now.getMinutes() / 60) * 360 + (now.getSeconds() / 60) * 6 + 90;
+  let hour =
+    ((now.getHours() % 12) / 12) * 360 + (now.getMinutes() / 60) * 30 + 90;
+
+  if (now.getSeconds() === 0) {
+    secondHand.style.transition =
+      "transform 0.0s cubic-bezier(0.97, 2.46, 0, 2.46)";
+  } else {
+    secondHand.style.transition =
+      "transform 0.05s cubic-bezier(0.97, 2.46, 0, 2.46)";
+  }
 
   secondHand.style.transform = `rotate(${seconds}deg)`;
-  hourHand.style.transform=`rotate(${hour}deg)`;
-  minuteHand.style.transform=`rotate(${minutes}deg)`;
-
+  minuteHand.style.transform = `rotate(${minutes}deg)`;
+  hourHand.style.transform = `rotate(${hour}deg)`;
 }
-
 setInterval(setTime, 1000);
