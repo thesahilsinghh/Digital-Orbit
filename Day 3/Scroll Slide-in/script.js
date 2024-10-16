da8e6620885ae6e0adc894df55fd26653b4c4f2f
Sahil Singh <97147722+thesahilsinghh@users.noreply.github.com>
2024-10-17 00:59:51 +0530

    Add files via upload

---
+++ Day 3/Scroll Slide-in/script.js
@@ -0,0 +1,21 @@
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function checkScroll(e) {
+  console.log(Number.parseInt(window.scrollY + window.innerHeight));
+}
+
+window.addEventListener("scroll", debounce(checkScroll));
